9f4aa39ed151fc62e5e50f652460434f6d191a01
saques <msaques@itba.edu.ar>
2018-01-28 17:49:21 -0300

    fix js compatibility issue

--- app/scripts/controllers/GameCtrl.js
+++ app/scripts/controllers/GameCtrl.js
@@ -1,7 +1,7 @@
 define(['paw2017a1frontend', 'directives/gameProfile'], function(paw2017a1frontend) {
 
     'use strict';
-    paw2017a1frontend.controller('GameCtrl', ['$scope', '$state','$log', '$stateParams', function($scope, $state, $log, $stateParams,) {
+    paw2017a1frontend.controller('GameCtrl', ['$scope', '$state','$log', '$stateParams', function($scope, $state, $log, $stateParams) {
         $scope.gameId = $stateParams.id;
     }]);
 });
