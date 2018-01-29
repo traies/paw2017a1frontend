5ee702ffef1bd08aa8e6a39d3a42e4feedb0e33f
saques <msaques@itba.edu.ar>
2018-01-29 18:25:16 -0300

    fixes in notifications and message tag links

--- app/scripts/directives/notifications.js
+++ app/scripts/directives/notifications.js
@@ -9,9 +9,10 @@ define(['paw2017a1frontend', 'services/sessionService', 'services/notificationsS
       scope: {},
       controller:
         ['$scope',
+				'$location',
         'sessionService',
         'notificationsService',
-        function($scope, session, notif){
+        function($scope, $location, session, notif){
 
           $scope.getNotifications = function(){
             return notif.getNotifications();
@@ -20,6 +21,22 @@ define(['paw2017a1frontend', 'services/sessionService', 'services/notificationsS
           $scope.updateNotifications = function(){
             notif.update();
           };
+
+					$scope.delete = function(id){
+						var i = 0;
+						var notifications = notif.getNotifications();
+						for(; i < notifications.length; i++){
+							if(notifications[i].id == id)
+								break;
+						}
+						notifications.splice(i, 1);
+						notif.delete(id);
+					};
+
+					$scope.goToUserProfile = function(id, tgtName){
+						$scope.delete(id);
+						$location.path('/user/' + tgtName + '/messages');
+					};
 
         }]
 		};
