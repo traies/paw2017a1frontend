f1b36419e79e955d5f37416d0afb82d52d36870f
traies <traies@itba.edu.ar>
2018-02-03 21:29:43 -0300

    add Reply posting and deleting

--- app/scripts/controllers/RepliesCtrl.js
+++ app/scripts/controllers/RepliesCtrl.js
@@ -1,10 +1,40 @@
 define(['paw2017a1frontend', 'directives/postView', 'services/MessageService'], function(paw2017a1frontend) {
 
     'use strict';
-    paw2017a1frontend.controller('RepliesCtrl', ['$scope', '$stateParams', 'MessageService', function($scope, $stateParams, MessageService) {
+    paw2017a1frontend.controller('RepliesCtrl', ['$scope', '$stateParams', 'MessageService', 'youtubePattern', function($scope, $stateParams, MessageService, youtubePattern) {
+        
         
         MessageService.idMessageResource().get({id: $stateParams.id}).$promise.then(function(data) {
             $scope.post = data;
+            $scope.setContentText = function (){
+                $scope.contentType = 'text';
+            };
+            $scope.setContentVideo = function (){
+                $scope.contentType = 'video';
+            };
+    
+    
+            $scope.submitReply= function (){
+                if($scope.replyForm.$valid) {
+                    MessageService.idMessageResource().reply({
+                        id: $stateParams.id,
+                        media: $scope.contentType,
+                        body: $scope.body
+                    }).$promise.then(function(data){
+                        $scope.postError = false;
+                        $scope.post.times_replied ++;
+                        $scope.replies.push(data);
+                        $scope.body = undefined;
+                        $scope.replyForm.$submitted = false;
+                    }, function(err){
+                        $scope.postError = true;
+                    });
+                }
+            }
+    
+            $scope.closeModal = function(){
+                $replyModal.close(true);
+            }
         }, function(error) {
             
         });
@@ -13,7 +43,25 @@ define(['paw2017a1frontend', 'directives/postView', 'services/MessageService'], 
             $scope.replies = data;
         }, function(error) {
 
-        })
+        });
+
+		$scope.contentType = 'text';
+		$scope.body = '';
+		$scope.youtubePattern = youtubePattern;
+        $scope.postError = false;
+        
+        $scope.deleteMessage = function(id, index){
+            MessageService.idMessageResource()
+            .delete({id: id})
+            .$promise.then(
+                function (data) {
+                    $scope.replies.splice(index, 1);
+                    $scope.post.times_replied--;
+                },
+                function (error){
+                }
+            );
+        };
     }]);
 
 });
